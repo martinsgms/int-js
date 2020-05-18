64c285d6e596e398e3f016481e6617fc3b42db54
Gabriel Martins <gabriel.santos@egs.com.br>
2020-05-18 18:13:08 -0300

    adiciona validação de uso para as classes e modifica o Helper para ser static

--- client/js/app/controller/NegociacaoController.js
+++ client/js/app/controller/NegociacaoController.js
@@ -10,16 +10,14 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
         
-        let dateHelper = new DateHelper();
-
         let negociacao = new Negociacao(
-            dateHelper.textToDate(this._inputData.value),
+            DateHelper.textToDate(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
             );
 
         console.log(negociacao);
-        console.log(dateHelper.dateToText(negociacao.data));
+        console.log(DateHelper.dateToText(negociacao.data));
     }
 
 }
--- client/js/app/helper/DateHelper.js
+++ client/js/app/helper/DateHelper.js
@@ -1,15 +1,21 @@
 class DateHelper {
 
-    textToDate(text) {
+    constructor() {
+        throw new Error("Esta classe só possui métodos estáticos");
+    }
+    
+    static textToDate(text) {
+        
+        if(!/\d{4}-\d{2}-\d{2}/.test(text))
+            throw new Error("Deve estar no formato yyyy-mm-dd");
+
         return new Date(...
             text
             .split("-")
             .map((item, index) => item - index % 2));
     }
 
-    dateToText(date) {
-        return date.getDate() + "/" +
-            (date.getMonth() + 1) + "/" +
-            date.getFullYear();
+    static dateToText(date) {
+        return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
     }
 }
