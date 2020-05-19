5e6c780c091dc2244992aa83ed69eb61d83b5c10
Gabriel Martins <gabriel.santos@egs.com.br>
2020-05-18 22:07:56 -0300

    implementa somatório com reduce

--- client/js/app/view/NegociacaoView.js
+++ client/js/app/view/NegociacaoView.js
@@ -19,18 +19,22 @@ class NegociacaoView {
                 <tbody>
                 </tbody>
 
-                ${model.negociacoes.map(n => {
-                    return `
+                ${model.negociacoes.map(n => 
+                    `
                         <tr>
                             <td>${DateHelper.dateToText(n.data)}</td>
                             <td>${n.quantidade}</td>
                             <td>${n.valor}</td>
                             <td>${n.volume}</td>
                         </tr>
-                    `;
-                }).join('')}
+                    `
+                ).join('')}
                 
                 <tfoot>
+                    <td colspan="3"></td>
+                    <td>
+                        ${model.negociacoes.reduce((total, n) => total += n.volume, 0.0)}
+                    </td>
                 </tfoot>
             </table>
         `;
